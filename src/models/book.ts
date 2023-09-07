11b013656427d8e95f681486ad333e0634196be6
Anthony Silva <thonycout@gmail.com>
2023-09-06 23:01:57 -0300

    poc 1

---
+++ src/models/book.ts
@@ -0,0 +1,56 @@
+export type Book = {
+  kind: string;
+  id: string;
+  etag: string;
+  selfLink: string;
+  volumeInfo: {
+    title: string;
+    authors: string[];
+    publishedDate: string;
+    description: string;
+    industryIdentifiers: {
+      type: string;
+      identifier: string;
+    }[];
+    readingModes: {
+      text: boolean;
+      image: boolean;
+    };
+    pageCount: number;
+    printType: string;
+    categories: string[];
+    averageRating: number;
+    ratingsCount: number;
+    maturityRating: string;
+    allowAnonLogging: boolean;
+    contentVersion: string;
+    language: string;
+    previewLink: string;
+    infoLink: string;
+    canonicalVolumeLink: string;
+  };
+  saleInfo: {
+    country: string;
+    saleability: string;
+    isEbook: boolean;
+  };
+  accessInfo: {
+    country: string;
+    viewability: string;
+    embeddable: boolean;
+    publicDomain: boolean;
+    textToSpeechPermission: string;
+    epub: {
+      isAvailable: boolean;
+    };
+    pdf: {
+      isAvailable: boolean;
+    };
+    webReaderLink: string;
+    accessViewStatus: string;
+    quoteSharingAllowed: boolean;
+  };
+  searchInfo: {
+    textSnippet: string;
+  };
+};
